83c8aa03e00da82ee8f35747f655d356a32643ed
Tushar <tusharshejwal1803@gmail.com>
2026-04-20 12:44:32 +0530

    add frontend meeting input

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from meeting_connector.connect_meeting import open_meeting
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "AI Meeting Assistant Running on Azure"
+    return render_template("index.html")
+
+@app.route("/join", methods=["POST"])
+def join():
+    link = request.form["meeting_link"]
+    open_meeting(link)
+    return f"Joining meeting: {link}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
